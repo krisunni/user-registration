a626e2608c7f183e25e66bf961d51b36463e51db
Kris Unnikannan <krish.unnikannan@gmail.com>
2019-08-25 22:23:54 -0400

    Prod Config with Postgress DB.

--- src/main/java/com/krisunni/user/config/SwaggerConfig.java
+++ src/main/java/com/krisunni/user/config/SwaggerConfig.java
@@ -13,6 +13,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
